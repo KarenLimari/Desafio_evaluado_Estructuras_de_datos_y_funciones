c6c6ef1abb0284a7113d537b90fc9d57f670602b
Karen Limari <karenlimari20@gmail.com>
2024-08-08 15:13:10 +0200

    primer commmit: variables listas

---
+++ conversiones.py
@@ -0,0 +1,7 @@
+import sys
+
+tasaconversion_solperuano = float(sys.argv[1])
+tasaconversion_pesoargentino = float(sys.argv[2])
+tasaconversion_dolaramericano = float(sys.argv[3])
+valorpesochileno_convertir = float(sys.argv[4])
+
